9705db0423109758ade6825991edee56f1b679b6
DanteManriqueOrellano <relaxurbano@me.com>
2021-04-01 13:27:45 -0500

    joder2

--- src/graphql/schemas/indexSchema.ts
+++ src/graphql/schemas/indexSchema.ts
@@ -1,9 +1,31 @@
-import { buildSchema } from "type-graphql";
+import { AuthChecker, buildSchema } from "type-graphql";
+import { Context } from "../resolvers/authorization/context.interface";
+//import { authChecker } from "../resolvers/authorization/auth-checker";
 import { clasesResolver } from "../resolvers/indexResolvers";
 
+const myauthChecker: AuthChecker<Context> = ({ context: { user } }, roles) => {
+    if (roles.length === 0) {
+      // if `@Authorized()`, check only if user exists
+      return user !== undefined;
+    }
+    // there are some roles defined now
+  
+    if (!user) {
+      // and if no user, restrict access
+      return false;
+    }
+    if (user.roles.some(role => roles.includes(role))) {
+      // grant access if the roles overlap
+      return true;
+    }
+  
+    // no roles matched, restrict access
+    return false;
+  };
 
 export const schemas:Promise<any> =  buildSchema(
     {
         resolvers: clasesResolver,
-        validate:false
+        validate:false,
+        authChecker:myauthChecker
     })
